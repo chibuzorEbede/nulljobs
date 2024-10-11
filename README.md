2882c636478b59a2193c536ff8f53de91587cf74
Chibuzor Ebede <chibuzorebede@gmail.com>
2024-10-10 22:05:28 -0400

    initial commit for nulljobs ui

---
+++ README.md
@@ -0,0 +1,14 @@
+# NullJobs
+A tiny job board web application
+
+## Purpose
+
+## Demo
+
+## Project Features
+
+## Technical Specifications
+
+## Dependencies
+
+## State Management
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import Layout from './components/layout'
+import AppBar from './components/app-bar'
+const App = ()=>{
+    return (
+        <Layout>
+            <AppBar/>
+            
+        </Layout>
+    )
+}
+
+
+export default App
