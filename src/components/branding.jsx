0c8e6e8a90169c7a722c4fc277d63243a5d4a8d2
Chibuzor Ebede <chibuzorebede@gmail.com>
2024-10-18 09:09:46 -0400

    Feat: Added vercel analytics and seo

---
+++ src/components/branding.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Branding = () => {
+  return (
+    <div>
+      <p className="text-xs text-center dark:text-rose-800">
+        Built by Chibuzor Ebede
+      </p>
+    </div>
+  );
+};
+
+export default Branding;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+import Branding from "./branding";
 
 const Layout = ({ children }) => {
-  return <div className=" bg-slate-50 dark:bg-slate-950 ">{children}</div>;
+  return (
+    <div className=" bg-slate-50 dark:bg-slate-950 ">
+      {children}
+      <Analytics />
+      <Branding />
+    </div>
+  );
 };
 
 export default Layout;
