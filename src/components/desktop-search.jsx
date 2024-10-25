9544147df86b5d7a8ba6de44a120f69ed7d3ddb4
Chibuzor Ebede <chibuzorebede@gmail.com>
2024-10-25 15:55:33 -0400

    modified the desktop search

--- src/components/desktop-search.jsx
+++ src/components/desktop-search.jsx
@@ -3,19 +3,23 @@ import { IoMdSearch } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 import Button from "./shared/button";
 
-const DesktopSearch = ({ onFilter }) => {
+const DesktopSearch = ({ data, onFilter }) => {
   //user input variables
   const IconSize = 20;
 
   return (
-    <div className="hidden md:flex p-5  gap-3 justify-evenly   items-center w-[90%] bg-white dark:bg-slate-900 text-rose-800 dark:text-rose-800 -mt-8 mx-auto rounded-md shadow-sm">
+    <div className="hidden md:flex p-5  gap-3 justify-normal   items-center w-[90%] bg-white dark:bg-slate-900 text-rose-800 dark:text-rose-800 -mt-8 mx-auto rounded-md shadow-sm">
       <IconTextInput
         icon={<IoMdSearch size={IconSize} />}
-        text={`filter by title, companies...`}
+        text={`filter by title, companies, expertise...`}
+        filterData={onFilter}
+        data={data}
       />
       <IconTextInput
         icon={<FaLocationDot size={IconSize} />}
         text={`filter by location`}
+        filterData={onFilter}
+        data={data}
       />
       <IconCheckInput />
     </div>
@@ -27,18 +31,21 @@ export default DesktopSearch;
 //local functions <Should extract these to their own files>
 
 //user text input
-const IconTextInput = ({ icon, text }) => {
+const IconTextInput = ({ icon, text, filterData, data }) => {
   //save the user input
   const [inputFieldText, setInputFieldText] = useState("");
 
   //function to handle updating of the input field
   const handleChange = (e) => {
+    console.log("in the handle change function");
     setInputFieldText(e.target.value);
     console.log("input is ", inputFieldText);
+    //call a function to update the data variable
+    filterData(data, inputFieldText);
   };
 
   return (
-    <div className="flex items-center justify-center border-r border-rose-800 bg-white  pl-2">
+    <div className="flex items-center justify border-r border-rose-800 bg-white  pl-2 w-[40%]">
       {icon}
       <input
         className="capitalize  p-2 outline-none placeholder:text-sm"
@@ -56,7 +63,7 @@ const IconTextInput = ({ icon, text }) => {
 //select job type
 const IconCheckInput = () => {
   return (
-    <div className="flex items-center justify-center ">
+    <div className="flex items-center justify-center w-[20%]">
       <input
         className="capitalize outline-none p-2  rounded-md"
         type="checkbox"
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -11,6 +11,7 @@ import axios from "axios";
 const Root = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
 
   //fetch the initial data once from the backend
   const liveUrl = `https://e1da64a0-e051-4459-a7d7-fecf8bbe0557-00-bwc9ui1rn0v4.spock.replit.dev/job-listings`;
@@ -19,10 +20,21 @@ const Root = () => {
   //data manipulation functions
 
   //take a data item and return the data filtered by the location parameter
-  const filterBasedOnUserInput = (data) => {
-    return data.filter((item) => item);
+  const filterBasedOnUserInput = (data, searchQuery) => {
+    console.log("in the filter function");
+    const filteredData = data.filter((itemData) =>
+      itemData.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+    );
+    console.log("filtered data is: ", filteredData);
+    //TODO
+    //find a way to update the dat with the new one
+    console.log("search query is ", searchQuery);
+    console.log("original data is ", data);
+
+    return filteredData;
   };
 
+  //set initial data on page load
   useEffect(() => {
     axios
       .get(liveUrl)
@@ -37,7 +49,7 @@ const Root = () => {
     <Layout>
       <AppBar />
       <FilterInput />
-      <DesktopSearch onFilter={filterBasedOnUserInput} />
+      <DesktopSearch data={data} onFilter={filterBasedOnUserInput} />
       {loading ? <Loading /> : <List data={data} />}
     </Layout>
   );
