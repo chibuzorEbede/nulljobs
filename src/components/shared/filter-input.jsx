f16a50f488164308e967e38ba216892e92790cfd
Chibuzor Ebede <chibuzorebede@gmail.com>
2025-03-20 19:27:34 -0400

    Feature: working on the filter component

--- src/components/shared/filter-input.jsx
+++ src/components/shared/filter-input.jsx
@@ -1,11 +1,14 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { BsFunnelFill } from "react-icons/bs";
 import { IoMdSearch } from "react-icons/io";
 
-const FilterInput = () => {
+const FilterInput = ({ filterBySearch }) => {
   const [isFiltered, setIsFiltered] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("Filter by title");
+  const [filteredData, setFilteredData] = useState([]);
 
-  //set the icn size in pixels
+  //set the icon size in pixels
   const IconSize = 26;
 
   //choose the type of filter
@@ -14,20 +17,45 @@ const FilterInput = () => {
     setIsFiltered(!isFiltered);
   };
 
+  //input handler functions
+  const handleSearchInputChange = (event) => {
+    //should add a debouncing function here?
+    setSearchTerm(event.target.value);
+  };
+
+  //search function
+  const handleSearch = () => {
+    //make a call to the api and pass the search term
+    axios
+      .get(`http://localhost:8000/jobs/${searchTerm}`)
+      .then((result) => {
+        setFilteredData(result.data);
+      })
+      .catch((err) => console.error(err));
+
+    console.log("filtered data is ", filteredData);
+    filterBySearch(filteredData);
+  };
+
   return (
     <div className="md:hidden p-5 flex  items-center w-[90%] bg-white  dark:bg-slate-900 dark:text-rose-800 -mt-8 mx-auto rounded-md shadow-sm">
       <input
         className="w-4/6 outline-none p-2  rounded-md text-rose-800 placeholder:text-sm"
         type="text"
-        name=""
-        id=""
+        name="filterTerm"
+        id="filterTerm"
         placeholder="Filter by title"
+        value={searchTerm}
+        onChange={handleSearchInputChange}
       />
       <aside className="flex justify-end gap-4 items-center   w-2/6">
         <div className="text-rose-800">
           <BsFunnelFill size={IconSize} onClick={setFilterType} />
         </div>
-        <div className="bg-rose-800 text-white dark:bg-white dark:text-rose-800 rounded-md p-2">
+        <div
+          onClick={handleSearch}
+          className="bg-rose-800 text-white dark:bg-white dark:text-rose-800 rounded-md p-2"
+        >
           <IoMdSearch size={IconSize} />
         </div>
       </aside>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,27 +5,41 @@ import FilterInput from "../components/shared/filter-input";
 import List from "../components/list";
 import DesktopSearch from "../components/desktop-search";
 import Loading from "../components/shared/loading";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 const Root = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  let isDataSet = useRef(false);
 
   //set the current url for use
   const liveUrl = `https://aab4b050-390e-4fac-8044-04dd579b7cad.us-east-1.cloud.genez.io/job-listings`;
   const localUrl = `http://localhost:8000/job-listings`;
 
+  const setFilterData = (filterData) => {
+    console.log("passed data is ", filterData);
+    return filterData;
+  };
+
   //set initial data on page load
   useEffect(() => {
-    axios
-      .get(localUrl)
-      .then((response) => {
-        setData(response.data);
-        setLoading(false);
-      })
-      .catch((err) => console.log(new Error(err)));
-  }, []);
+    if (isDataSet.current === true) {
+      setData(setFilterData);
+      return;
+    } else {
+      console.log("runing the axios first fectch");
+      axios
+        .get(localUrl)
+        .then((response) => {
+          setData(response.data);
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
+    }
+
+    return () => (isDataSet.current = true);
+  }, [data]);
 
   if (!data) {
     throw new Error("please check connectivity, no data received");
@@ -33,7 +47,7 @@ const Root = () => {
   return (
     <Layout>
       <AppBar />
-      <FilterInput />
+      <FilterInput filterBySearch={setFilterData} />
       <DesktopSearch data={data} />
       {loading ? <Loading /> : <List data={data} />}
     </Layout>
